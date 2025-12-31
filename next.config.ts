00498804f575869c31776e4b89dcfddd898fd8f5
Nguyen Van Hieu <nguyenvanhieu36@gmail.com>
2025-12-31 23:05:48 +0700

    Fix GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Bắt buộc có dòng này để chạy trên GitHub Pages
+  images: {
+    unoptimized: true, // Thêm dòng này nếu bạn có dùng thẻ <Image />
+  },
 };
 
 export default nextConfig;
